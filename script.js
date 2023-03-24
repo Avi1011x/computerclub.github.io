27f5127e44992c1cb343823a35b5d1150674245a
Avi1011x <bhavish.seetul1@umail.uom.ac.mu>
2023-03-24 15:51:17 +0400

    wa

---
+++ script.js
@@ -0,0 +1,31 @@
+// set the date we're counting down to
+var target_date = new Date('April, 17, 2023').getTime();
+ 
+// variables for time units
+var days, hours, minutes, seconds;
+ 
+// get tag element
+var countdown = document.getElementById('countdown');
+ 
+// update the tag with id "countdown" every 1 second
+setInterval(function () {
+ 
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+ 
+    // do some time calculations
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+     
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+     
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+     
+    // format countdown string + set tag value
+    countdown.innerHTML = '<span class="days">' + days +  ' <label>Days</label></span> <span class="hours">' + hours + ' <label>Hours</label></span> <span class="minutes">'
+    + minutes + ' <label>Minutes</label></span>';  
+ 
+}, 1000);
